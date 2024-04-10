fad47c16f50e58ea7fa65f6dc94776646c1edb1d
evavelasco <evapibiyal@gmail.com>
2024-04-10 15:22:12 +0200

    header y footer sin rematar

--- frontend/src/middleware/router/AppRouter.jsx
+++ frontend/src/middleware/router/AppRouter.jsx
@@ -1,12 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { Home } from "../../core/pages/Home/Home";
-
+import { Header } from "../../core/components/Header/Header";
+import { Footer } from "../../core/components/Footer/Footer";
 export const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Header />
+      <Outlet />
+      <Footer />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
