a58f0c38496fd3b321814f41fafdc416d3efa141
herminiocastro <hxssport@gmail.com>
2024-04-10 17:12:03 +0200

    empezado componente y estilos de aboutus

--- frontend/src/core/components/AboutUs/AboutUs.jsx
+++ frontend/src/core/components/AboutUs/AboutUs.jsx
@@ -2,79 +2,90 @@ import "../AboutUs/AboutUs.css";
 
 export const AboutUs = () => {
   return (
-    <div className="quienes-somos">
-      <div className="section">
-        <img src="imagen1.jpg" alt="Imagen 1" className="round-image" />
-        <div className="info">
-          <h2>PATRICIA ACEBES TAMARGO</h2>
-          <p>
-            Fundadora responsable de AsturiasAi .Es economista cuantitativa, con
-            formación de postgrado inteligencia artificial, blockchain e
-            actualmente iniciándose en temas de computación cuántica y
-            doctoranda en la universidad de Alcalá en temas de sostenibilidad
-            ambiental y criptomonedas, además de disponer de un especial
-            interés en el estudio del análisis de la microbiota
-            intestinal(bacterias tenemos en el intestino) sobre la salud
-            utilizando la metodología de machine learning (data / ia).Dispone
-            de más de seis años en el mundo tecnológico. Tanto en consultoría
-            tecnológica, startups, cómo durante los últimos años en un centro
-            tecnológico. Sus funciones han ido ligadas tanto en lo que concierne
-            a labores de data science,hasta labores de gestión de proyectos
-            nacionales y europeos(ya fueran búsqueda de partners, convocatorias,
-            paquetes de trabajo, ) y de difusión de la tecnología de
-            inteligencia artificial y blockchain. Anteriormente ha desempeñado
-            funciones tanto en el sector financiero, cómo de la administración
-            pública.
-          </p>
-          <a href="https://www.linkedin.com/in/patriciaacebestamargo/"></a>
+    <div>
+      <h1 className="title">QUIENES SOMOS</h1>
+      <div className="quienes-somos">
+        <div className="section">
+          <div className="content">
+            <img src="imagen1.jpg" alt="Imagen 1" className="round-image" />
+            <h2>PATRICIA ACEBES TAMARGO</h2>
+          </div>
+          <div className="info1">
+            <p>
+              Fundadora responsable de AsturiasAi .Es economista cuantitativa,
+              con formación de postgrado inteligencia artificial, blockchain e
+              actualmente iniciándose en temas de computación cuántica y
+              doctoranda en la universidad de Alcalá en temas de sostenibilidad
+              ambiental y criptomonedas, además de disponer de un especial
+              interés en el estudio del análisis de la microbiota
+              intestinal(bacterias tenemos en el intestino) sobre la salud
+              utilizando la metodología de machine learning (data / ia).Dispone
+              de más de seis años en el mundo tecnológico. Tanto en consultoría
+              tecnológica, startups, cómo durante los últimos años en un centro
+              tecnológico. Sus funciones han ido ligadas tanto en lo que
+              concierne a labores de data science,hasta labores de gestión de
+              proyectos nacionales y europeos(ya fueran búsqueda de partners,
+              convocatorias, paquetes de trabajo, ) y de difusión de la
+              tecnología de inteligencia artificial y blockchain. Anteriormente
+              ha desempeñado funciones tanto en el sector financiero, cómo de la
+              administración pública.
+            </p>
+            <a href="https://www.linkedin.com/in/patriciaacebestamargo/" className="linkedin1"></a>
+          </div>
         </div>
-      </div>
-      <hr />
-      <div className="section2">
-        <div className="info">
-          <h2>BEATRIZ INFANZÓN</h2>
-          <p>
-            Divulgadora y redactora de contenidos tecnológicos. Se ha
-            especializado en tecnologías emergentes como blockchain, Web3,
-            criptoeconomía, inteligencia artificial, IoT y Big Data.Facilita
-            que los términos tecnológicos sean más fáciles de entender a través
-            de contenidos, que crea para sus clientes y también para sus
-            seguidores. Prueba de ello es Stories from the Web3, un canal de
-            Youtube y una newsletter semanal, donde comparte noticias de Web3 y
-            todo lo que va descubriendo en su día a día como creadora de
-            contenido.{" "}
-          </p>
-          <a href="https://www.linkedin.com/in/beatrizinfanzon/"></a>
+        <hr />
+        <div className="section2">
+          <div className="info2">
+            <p>
+              Divulgadora y redactora de contenidos tecnológicos. Se ha
+              especializado en tecnologías emergentes como blockchain, Web3,
+              criptoeconomía, inteligencia artificial, IoT y Big Data.Facilita
+              que los términos tecnológicos sean más fáciles de entender a
+              través de contenidos, que crea para sus clientes y también para
+              sus seguidores. Prueba de ello es Stories from the Web3, un canal
+              de Youtube y una newsletter semanal, donde comparte noticias de
+              Web3 y todo lo que va descubriendo en su día a día como creadora
+              de contenido.{" "}
+            </p>
+            <a href="https://www.linkedin.com/in/beatrizinfanzon/"></a>
+          </div>
+          <div className="content">
+            <img src="imagen2.jpg" alt="Imagen 2" className="round-image" />
+            <h2>BEATRIZ INFANZÓN</h2>
+          </div>
         </div>
-        <img src="imagen2.jpg" alt="Imagen 2" className="round-image" />
-      </div>
-      <hr />
-      <div className="section3">
-        <img src="imagen3.jpg" alt="Imagen 3" className="round-image" />
-        <div className="info">
-          <h2>PABLO VIEJO</h2>
-          <p>
-            Fundador y CEO de Bettermaps.AI, se especializa en el desarrollo de
-            proyectos con inteligencia artificial (IA) y tecnologías
-            geoespaciales, tanto en desarrollo de productos como en consultoría.
-            Su experiencia incluye la implementación de IA y modelado 3D para
-            análisis y gestión espacial. Como CEO de PTGEM, lideró el desarrollo
-            de soluciones innovadoras para el Internet de las Cosas (IoT),
-            integrando tecnologías web 3D y análisis geoespacial. Pablo
-            participa activamente en proyectos de investigación y desarrollo,
-            aplicando la IA para impulsar el avance de tecnologías digitales.
-            Además de su rol en Bettermaps.AI, Pablo Viejo ha tenido una carrera
-            destacada en el ámbito internacional, marcada por posiciones de
-            liderazgo en diversas geografías, desde Singapur hasta España.
-            Durante su estancia en Singapur, se desempeñó como Administrador
-            General de EDF Lab Singapore y Jefe del Centro de Excelencia de
-            Ciudades Sostenibles de EDF, donde lideró proyectos innovadores en
-            tecnologías sostenibles y digitales para ciudades. Pablo posee una
-            formación académica sólida con un título en Arquitectura de la
-            Universität Karlsruhe (TH) y programas especializados en Sistemas
-            Complejos.
-          </p>
-          <a href="https://www.linkedin.com/in/pablo-viejo/"></a>
+        <hr />
+        <div className="section3">
+          <div className="content">
+            <img src="imagen3.jpg" alt="Imagen 3" className="round-image" />
+            <h2>PABLO VIEJO</h2>
+          </div>
+          <div className="info3">
+            <p>
+              Fundador y CEO de Bettermaps.AI, se especializa en el desarrollo
+              de proyectos con inteligencia artificial (IA) y tecnologías
+              geoespaciales, tanto en desarrollo de productos como en
+              consultoría. Su experiencia incluye la implementación de IA y
+              modelado 3D para análisis y gestión espacial. Como CEO de PTGEM,
+              lideró el desarrollo de soluciones innovadoras para el Internet de
+              las Cosas (IoT), integrando tecnologías web 3D y análisis
+              geoespacial. Pablo participa activamente en proyectos de
+              investigación y desarrollo, aplicando la IA para impulsar el
+              avance de tecnologías digitales. Además de su rol en
+              Bettermaps.AI, Pablo Viejo ha tenido una carrera destacada en el
+              ámbito internacional, marcada por posiciones de liderazgo en
+              diversas geografías, desde Singapur hasta España. Durante su
+
+              estancia en Singapur, se desempeñó como Administrador General de
+              EDF Lab Singapore y Jefe del Centro de Excelencia de Ciudades
+              Sostenibles de EDF, donde lideró proyectos innovadores en
+              tecnologías sostenibles y digitales para ciudades. Pablo posee una
+              formación académica sólida con un título en Arquitectura de la
+              Universität Karlsruhe (TH) y programas especializados en Sistemas
+              Complejos.
+            </p>
+            <a href="https://www.linkedin.com/in/pablo-viejo/"></a>
+          </div>
         </div>
       </div>
     </div>
