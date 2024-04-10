f887ce7f00065884bf8a61816e6a6198752312de
herminiocastro <hxssport@gmail.com>
2024-04-10 15:40:40 +0200

    empezado componente y estilos de aboutus

--- frontend/src/middleware/router/AppRouter.jsx
+++ frontend/src/middleware/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../../core/pages/Home/Home";
+import { AboutUs } from "../../core/components/AboutUs/AboutUs"
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/aboutus/*" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
   )
