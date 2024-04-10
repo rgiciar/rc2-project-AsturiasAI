1d1207aecfd1590854c63cdc44ab6bd9e59d16c2
Anadevega74 <151641554+Anadevega74@users.noreply.github.com>
2024-04-10 14:22:38 +0200

    Card jsx added

--- frontend/src/core/components/Card/Card.jsx
+++ frontend/src/core/components/Card/Card.jsx
@@ -1,1 +1,19 @@
+import { useRef, useState } from "react";
+import './Card.css';
 
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className="card">
+      <img src="imagen.jpg" alt="Imagen de la entrada" />
+      <div className="contenido">
+        <p className="fecha">Fecha de publicación</p>
+        <h3>Título de la entrada</h3>
+        <p>Descripción de la entrada...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
