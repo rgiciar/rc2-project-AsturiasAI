e5a789e2c73d7761bf8b4f2d0e34479c9e83cdbb
Anadevega74 <151641554+Anadevega74@users.noreply.github.com>
2024-04-11 14:17:27 +0200

    Subida tarjeta de blog

--- frontend/src/core/components/Card/Card.jsx
+++ frontend/src/core/components/Card/Card.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import "./Card.css";
 
-const Card = ({ titulo, fecha, imagen, descripcion, link }) => {
+import imagenRobot from './img/robot.jpg';
+
+
+export const Card = () => {
   return (
     <div className="card">
-      <h2 className="titulo">{titulo}</h2>
-      <p className="fecha">{fecha}</p>
-      <img className="imagen" src={imagen} alt={titulo} />
-      <p className="descripcion">{descripcion}</p>
-      <a className="link" href={link}>Leer más</a>
+      <h2 className="titulo">Título de entrada</h2>
+      <p className="fecha">20/03/2024</p>
+      <img className="imagen" src={imagenRobot} alt="Ïmagen de entrada" />
+      <p className="descripcion">Hola hola esto es una descripción del blog</p>
+      <a className="link" href="">
+        Leer más
+      </a>
     </div>
   );
 };
-
-export default Card;
--- frontend/src/middleware/router/AppRouter.jsx
+++ frontend/src/middleware/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Home } from "../../core/pages/Home/Home";
 import { Header } from "../../core/components/Header/Header";
 import { Footer } from "../../core/components/Footer/Footer";
 import { Contact } from "../../core/components/Contact/Contact";
+import { Blog } from "../../core/pages/Blog/Blog";
 
 export const AppRouter = () => {
   return (
@@ -13,6 +14,7 @@ export const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </BrowserRouter>
   );
