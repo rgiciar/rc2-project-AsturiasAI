91e16b5336661c3bc7478b944348fb4f3aa43bbc
SrB4by <oliversanchezcasiano@gmail.com>
2024-04-11 12:27:42 +0200

    add center newsletter

--- frontend/src/core/components/NewsLetter/NewsLetter.jsx
+++ frontend/src/core/components/NewsLetter/NewsLetter.jsx
@@ -1,14 +1,19 @@
-
+import "/src/core/components/NewsLetter/NewsLetter.css";
 
 export const NewsLetter = () => {
     return (
-        <iframe 
-        className='newsLetter'
-        src="https://oliversanchezcasiano.substack.com/embed" 
-        width="480" 
-        height="320" 
-        frameborder="no" 
-        scrolling="no">
-        </iframe>
+        <div className='newsLetter'>
+            <h2 className="tittle-newsletter">
+                Subscribete a nuestro NewsLetter
+            </h2>
+            <iframe
+
+                src="https://oliversanchezcasiano.substack.com/embed"
+                width="480"
+                height="150"
+                frameborder="no"
+                scrolling="no">
+            </iframe>
+        </div>
     );
-  };
+};
