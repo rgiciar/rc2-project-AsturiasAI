67cac84116aa253b56f0c5d3d7f4b377cff181cb
Anadevega74 <151641554+Anadevega74@users.noreply.github.com>
2024-04-11 15:53:44 +0200

    First card touch-ups

--- frontend/src/core/components/Card/Card.jsx
+++ frontend/src/core/components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import "./Card.css";
 
+
 import imagenKitchen from './img/kitchen.jpg';
 
 
@@ -9,11 +10,12 @@ export const Card = () => {
       <img className="imagen" src={imagenKitchen} alt="Ïmagen de entrada" />
       <p className="fecha">2 de marzo de 2024</p>
       <h2 className="titulo">IA HASTA EN LA SOPA</h2>
-      <p className="descripcion">La IA está revolucionando la cocina. Un robot chef prepara deliciosas recetas con solo tocar un botón. puede cocinar de forma independiente, sin necesidad de intervención humana.
- </p>
+      <p className="descripcion">La IA está revolucionando la cocina. Un robot chef prepara deliciosas recetas con solo tocar un botón. puede cocinar de forma independiente, sin necesidad de intervención humana.</p>
       <a className="link" href="">
         Leer más
       </a>
     </div>
+
+    
   );
 };
