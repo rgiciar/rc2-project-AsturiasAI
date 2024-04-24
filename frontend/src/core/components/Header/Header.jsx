211c06fbee15bcbccd6084857d7c090e26e801cd
SrB4by <oliversanchezcasiano@gmail.com>
2024-04-24 11:44:51 +0200

    vuelta a los cambios de blog de Oli

--- frontend/src/core/components/Header/Header.jsx
+++ frontend/src/core/components/Header/Header.jsx
@@ -35,7 +35,7 @@ export const Header = () => {
             </Link>
           </li>
           <li>
-            <Link smooth to="/blog">
+            <Link smooth to="/blogList">
               Blog
             </Link>
           </li>
