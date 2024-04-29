8dc567cc7be5a4833a78ff1b940d078dfb97c617
SrB4by <oliversanchezcasiano@gmail.com>
2024-04-29 12:29:31 +0200

    create new test for the form contact

--- frontend/src/tests/AboutUs.test.jsx
+++ frontend/src/tests/AboutUs.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
 import { AboutUs } from "../core/components/AboutUs/AboutUs.jsx";
+import { FormContact } from "../core/components/FormContact/FormContact.jsx";
+import userEvent from '@testing-library/user-event';
 
 
 test('Aparece el nombre de cada uno de los colaboradores', () => {
@@ -13,7 +15,21 @@ test('Aparece el nombre de cada uno de los colaboradores', () => {
 });
 
 
+test('El formulario de contacto envía los datos correctamente', async () => {
+    render(<FormContact />);
+    
+    
+    await userEvent.type(screen.getByPlaceholderText('EMAIL'), 'test@example.com');
+    await userEvent.type(screen.getByPlaceholderText('NOMBRE'), 'Juan Perez');
+    await userEvent.type(screen.getByPlaceholderText('ASUNTO'), 'Consulta General');
+    await userEvent.type(screen.getByPlaceholderText('MENSAJE'), 'Hola, quisiera obtener más información acerca de sus servicios.');
 
+    
+    await userEvent.click(screen.getByRole('button', { name: 'Enviar' }));
+
+    
+    expect(await screen.findByText('Enviado Correctamente! 📩')).toBeInTheDocument();
+});
 
 
     
