0f8506d6afa903778f33561642aba2c9fd4ee084
herminiocastro <hxssport@gmail.com>
2024-05-24 13:15:51 +0200

    cambios en src substrack

--- frontend/src/core/components/NewsLetter/NewsLetter.jsx
+++ frontend/src/core/components/NewsLetter/NewsLetter.jsx
@@ -1,19 +1,14 @@
 import "/src/core/components/NewsLetter/NewsLetter.css";
 
 export const NewsLetter = () => {
-    return (
-        <div className='newsLetter'>
-            <h2 className="tittle-newsletter">
-                Subscribete a nuestro NewsLetter
-            </h2>
-            <iframe
-                src="https://oliversanchezcasiano.substack.com/embed"
-                width="400"
-                height="150"
-                // frameborder="no"
-                // scrolling="no"
-                >
-            </iframe>
-        </div>
-    );
+  return (
+    <div className="newsLetter">
+      <h2 className="tittle-newsletter">Subscribete a nuestro NewsLetter</h2>
+      <iframe
+        src="https://asturiasai.substack.com/embed"
+        width="400"
+        height="150"
+      ></iframe>
+    </div>
+  );
 };
